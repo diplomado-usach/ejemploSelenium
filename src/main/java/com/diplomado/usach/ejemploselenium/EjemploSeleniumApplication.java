4b306f6c3ed58ff4631e0bf8f6cd91254e3607d8
Ricardo Quiroga <ricardo.s.quiroga@gmail.com>
2022-03-18 19:42:59 -0300

    first commit

---
+++ src/main/java/com/diplomado/usach/ejemploselenium/EjemploSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.diplomado.usach.ejemploselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSeleniumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemploSeleniumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diplomado/usach/ejemploselenium/EjemploSeleniumApplicationTests.java
@@ -0,0 +1,41 @@
+package com.diplomado.usach.ejemploselenium;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class EjemploSeleniumApplicationTests {
+
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("Iniciando configuración...");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        driver.manage().window().maximize();
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+        driver.navigate().to("https://www.google.com");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Iniciando Pruebas...");
+        WebElement searchbox = driver.findElement(By.name("q"));
+        searchbox.sendKeys("HandBook Devops");
+        searchbox.submit();
+        assertEquals("HandBook Devops - Buscar con Google", driver.getTitle());
+    }
+
+}
